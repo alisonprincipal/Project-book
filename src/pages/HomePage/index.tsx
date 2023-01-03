830d5ddab7adb1049ee26b0283b99b1212d5547f
alison <alison-bh@hotmail.com>
2023-01-03 15:00:30 -0300

    feat:cria branch develop

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -4,7 +4,7 @@ export const HomePage =()=>{
     return(
         <>
         <Header rota1="/login" nameRota1="Login" rota2="/Register" nameRota2="Cadastrar"/>
-        <h1>PAGINA HOME PAGE</h1>
+        <h1>PAGINA HOME PAGEE</h1>
         </>
     )
 }
