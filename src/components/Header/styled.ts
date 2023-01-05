11f66df9816f86858a6a6134bf879123aeff6eae
alison <alison-bh@hotmail.com>
2023-01-05 13:05:14 -0300

    fix:bug responsividade

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -7,26 +7,40 @@ export const StyleHeader = styled.header`
     align-items: center;
     justify-content: space-between;
     padding: 20px 30px;
-    background: #E0DDDD;
+    background:  #FFFFFF;
+    position: fixed;
+    top: 0;
+    left: 0;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+   
 
-    @media(max-width:315px){
+    @media(max-width:321px){
         flex-wrap: wrap;
         gap: 15px;
         justify-content: center; 
     }
 
-
+    h1{
+            font-weight: 400;
+            font-size: 22px;
+            line-height: 27px;
+            color: #181EBE;
+        }
     div{
         display: flex;
-        gap: 20px;
-
+        width: 170px;
+        justify-content: space-between;
         a{
             text-decoration: none;
             font-weight: 400;
             font-size: 18px;
             line-height: 22px;
             color: #000000;
+
+            :hover{
+                font-size: 20px;
+                text-decoration: underline;
+            }
         }
     }
-
 `
